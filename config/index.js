177c83db0840f7614943e9a3322f94e9852dfc5c
buzihao <82298636+buzihao@users.noreply.github.com>
2023-06-07 19:26:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wx0e949a072487642d",
+  appsecret: "1ec957761fdc59a9bdadec2dcf91b206",
+  template_id: "VIdmongv-bfumq7f4ih9J9qIqySrTBNbN-FXW3voCDw",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "南京",
+  first_date: "2017-12-24",
 };
